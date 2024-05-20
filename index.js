49b59983ea38c37396d17bc0582f7f035834a5da
alhamda50 <alhamdaalhamda5@gmail.com>
2024-05-20 21:41:52 +0530

    more files added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 // Database
@@ -9,6 +12,9 @@ const booky = express();
 
 booky.use(bodyParser.urlencoded({ extended: true }));
 booky.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("connection established"));
 
 /*
 Route                 /
